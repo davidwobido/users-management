3efd6616419706a003d4eb44cefd479cb92d25b7
davidwobido <davidwobido@googlemail.com>
2021-10-26 16:59:30 +0200

    :sparkles: Add post feature

--- src/server.ts
+++ src/server.ts
@@ -3,28 +3,35 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+// For parsing application/json
+app.use(express.json());
+
 const users = ['Alice', 'Anke', 'David', 'Zied'];
 
+app.post('/api/users', (request, response) => {
+  response.send(request.body.name);
+});
+
 app.delete('/api/users/:name', (request, response) => {
-  const isNameKnown = users.includes(request.params.name);
-  if (isNameKnown) {
-    const usersIndex = users.indexOf(request.params.name);
-    users.splice(usersIndex);
-    response.send(
-      'User ' + request.params.name + ' deleted. Still in the list: ' + users
-    );
-  } else {
-    response.status(404).send('User does not exist');
+  const usersIndex = users.indexOf(request.params.name);
+  if (usersIndex === -1) {
+    response.status(404).send("User doesn't exist. Check another Castle 🏰");
+    return;
   }
+  users.splice(usersIndex, 1);
+  response.send('Deleted');
 });
 
 // app.delete('/api/users/:name', (request, response) => {
 //   const isNameKnown = users.includes(request.params.name);
 //   if (isNameKnown) {
-//     delete request.params.name;
-//     response.send('User deleted');
+//     const usersIndex = users.indexOf(request.params.name);
+//     users.splice(usersIndex);
+//     response.send(
+//       'User ' + request.params.name + ' deleted. Still in the list: ' + users
+//     );
 //   } else {
-//     response.send('User does not exist');
+//     response.status(404).send('User does not exist');
 //   }
 // });
 
