3480321b4ca072ea9775f69721a935581fa18ae7
davidwobido <davidwobido@googlemail.com>
2021-10-26 16:37:58 +0200

    :sparkles: Add delete function

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,37 @@ const port = 3000;
 
 const users = ['Alice', 'Anke', 'David', 'Zied'];
 
+app.delete('/api/users/:name', (request, response) => {
+  const isNameKnown = users.includes(request.params.name);
+  if (isNameKnown) {
+    const usersIndex = users.indexOf(request.params.name);
+    users.splice(usersIndex);
+    response.send(
+      'User ' + request.params.name + ' deleted. Still in the list: ' + users
+    );
+  } else {
+    response.status(404).send('User does not exist');
+  }
+});
+
+// app.delete('/api/users/:name', (request, response) => {
+//   const isNameKnown = users.includes(request.params.name);
+//   if (isNameKnown) {
+//     delete request.params.name;
+//     response.send('User deleted');
+//   } else {
+//     response.send('User does not exist');
+//   }
+// });
+
 app.get('/api/users/:name/', (request, response) => {
   const isNameKnown = users.includes(request.params.name);
   if (isNameKnown) {
     response.send(request.params.name);
   } else {
-    response.status(404).send('Can’t find name');
+    response.status(404).send('User does not exist');
   }
 });
-
-// app.get('/api/users/:name/', (request, response) => {
-//   response.send(request.params.name);
-// });
 
 app.get('/api/users/', (_request, response) => {
   response.send(users);
